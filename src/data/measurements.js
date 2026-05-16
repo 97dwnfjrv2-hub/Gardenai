8a55494093c109c04986abaa173e754d66923200
97dwnfjrv2-hub <97dwnfjrv2@privaterelay.appleid.com>
2026-05-16 16:37:05 +1000

    Add seeded measurements

---
+++ src/data/measurements.js
@@ -0,0 +1,5 @@
+export const initialMeasurements = [
+  { id: 'm-1', runId: 'run-03', day: 'Flower D09', drybackPct: 24, runoffEc: 2.82, runoffPh: 6.0, rootZoneVwc: 34 },
+  { id: 'm-2', runId: 'run-03', day: 'Flower D10', drybackPct: 27, runoffEc: 2.91, runoffPh: 6.0, rootZoneVwc: 32 },
+  { id: 'm-3', runId: 'run-03', day: 'Flower D11', drybackPct: 26, runoffEc: 3.0, runoffPh: 6.1, rootZoneVwc: 31.8 },
+]
